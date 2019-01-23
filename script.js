f2bc0957752aa149375faf5e29181097d7583086
Tony Gustafsson <mail@tonyg.se>
2019-01-23 19:59:42 +0100

    Prettier code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function game() {
-    "use strict";
+    'use strict';
 
     var input = document.getElementById('input'),
         form = document.getElementById('footer-form'),
@@ -11,10 +11,10 @@
     input.focus();
 
     var state = {
-        "place": "meadow",
-        "description": "You feel green grass under you bare foots. You seem to be on some kind of meadow. No one is around, and nature makes almost no sounds.",
-        "commands": [ "stand", "forest", "describe" ],
-        "action": "laying"
+        place: 'meadow',
+        description: 'You feel green grass under you bare foots. You seem to be on some kind of meadow. No one is around, and nature makes almost no sounds.',
+        commands: ['stand', 'forest', 'describe'],
+        action: 'laying'
     };
 
     (function stayOnBottom() {
@@ -27,12 +27,12 @@
         var oldestParagraph = document.getElementById('answer-' + (answerId - 50));
         if (oldestParagraph) answers.removeChild(oldestParagraph);
 
-        var delay = Math.floor((Math.random() * 750) + 1);
+        var delay = Math.floor(Math.random() * 750 + 1);
 
-        var soundFile = 'sounds/answer' + (Math.floor((Math.random() * 3) + 1)) + '.mp3';
+        var soundFile = 'sounds/answer' + Math.floor(Math.random() * 3 + 1) + '.mp3';
         var audio = new Audio(soundFile);
 
-        setTimeout(function () {
+        setTimeout(function() {
             audio.play();
         }, delay);
 
@@ -41,12 +41,12 @@
         answers.appendChild(newParagraph);
 
         Typed.new('#answer-' + answerId, {
-			strings: [html],
-			typeSpeed: 0,
+            strings: [html],
+            typeSpeed: 0,
             startDelay: delay,
             showCursor: false,
             contentType: 'html'
-		});
+        });
 
         answerId++;
     }
@@ -54,21 +54,21 @@
     function shake() {
         body.className = 'shake';
 
-        setTimeout(function () {
+        setTimeout(function() {
             body.className = '';
         }, 2000);
     }
 
-	document.addEventListener("DOMContentLoaded", function (){
-		say('Whe...^500 where am I? Am I... ^750 laying down?!');
+    document.addEventListener('DOMContentLoaded', function() {
+        say('Whe...^500 where am I? Am I... ^750 laying down?!');
 
         var soundFile = 'sounds/music.mp3';
         var audio = new Audio(soundFile);
         audio.loop = true;
         audio.play();
-	});
+    });
 
-    document.addEventListener('click', function () {      
+    document.addEventListener('click', function() {
         input.focus();
     });
 
@@ -77,82 +77,73 @@
     }
 
     function changeBackground(place) {
-        blackOverlay.className = "overlay black visible";
+        blackOverlay.className = 'overlay black visible';
 
-        setTimeout(function () {
+        setTimeout(function() {
             body.style.backgroundImage = 'url("img/' + place + '.jpg")';
-            blackOverlay.className = "overlay black";
+            blackOverlay.className = 'overlay black';
         }, 1000);
     }
 
-    form.addEventListener('submit', function (e) {
+    form.addEventListener('submit', function(e) {
         e.preventDefault();
 
         if (input.value == 'hello' || input.value == 'hi') {
             say('Hello young sir.');
-        }
-        else if (input.value == 'describe' || input.value == 'd') {
+        } else if (input.value == 'describe' || input.value == 'd') {
             say(state.description);
-        }
-        else if (input.value == 'stand' && state.action == "laying") {
-            state.action = "standing";
+        } else if (input.value == 'stand' && state.action == 'laying') {
+            state.action = 'standing';
 
-            changeBackground("meadow-standing");
+            changeBackground('meadow-standing');
 
-            say("You stand up.");
-        }
-        else if (input.value == 'meadow' && state.place == "forest") {
-            state.place = "meadow";
-            state.description = "You feel green grass under you bare foots. You seem to be on some kind of meadow. No one is around, and nature makes almost no sounds.";
-            state.commands = ["forest", "description"];
+            say('You stand up.');
+        } else if (input.value == 'meadow' && state.place == 'forest') {
+            state.place = 'meadow';
+            state.description =
+                'You feel green grass under you bare foots. You seem to be on some kind of meadow. No one is around, and nature makes almost no sounds.';
+            state.commands = ['forest', 'description'];
 
-            changeBackground("meadow-standing");
+            changeBackground('meadow-standing');
 
             say(state.description);
-        }
-        else if (input.value == 'forest' && state.place == "meadow") {
-            state.place = "forest";
+        } else if (input.value == 'forest' && state.place == 'meadow') {
+            state.place = 'forest';
             state.description = "You are now in a dark forrest. Are you sure that's wise at this hour?";
-            state.commands = ["meadow", "talk", "description"];
+            state.commands = ['meadow', 'talk', 'description'];
 
-            changeBackground("forest");
+            changeBackground('forest');
 
             say(state.description);
-        }
-        else if (input.value == 'talk' && state.place == "forest") {
+        } else if (input.value == 'talk' && state.place == 'forest') {
             shake();
-            say("You begin to speak, and suddently you feel the ground moving...");
-        }
-        else if (input.value == 'clear') {
+            say('You begin to speak, and suddently you feel the ground moving...');
+        } else if (input.value == 'clear') {
             answers.innerHTML = '';
-        }
-        else if (input.value == 'help' || input.value == 'commands') {
-            var commands = "";
+        } else if (input.value == 'help' || input.value == 'commands') {
+            var commands = '';
 
             for (var i in state.commands) {
-                commands += "<li>" + state.commands[i] + "</li>";
+                commands += '<li>' + state.commands[i] + '</li>';
             }
 
             say('Possible commands are:<br><ul>' + commands + '</ul>');
-        }
-        else if (input.value == 'quit' || input.value == 'exit') {
+        } else if (input.value == 'quit' || input.value == 'exit') {
             say('Actually, you there is now where go to.');
-        }
-        else if (input.value == 'ok' || input.value == 'OK') {
+        } else if (input.value == 'ok' || input.value == 'OK') {
             say('...');
-        }
-        else {
-            var randomAnswer = Math.floor((Math.random() * 3) + 1);
+        } else {
+            var randomAnswer = Math.floor(Math.random() * 3 + 1);
 
             switch (randomAnswer) {
                 case 1:
-                    say('Excuse me? Type \'help\' if you need to.');
+                    say("Excuse me? Type 'help' if you need to.");
                     break;
                 case 2:
-                    say('Come again, please? Type \'help\' if you need to.');
+                    say("Come again, please? Type 'help' if you need to.");
                     break;
-                case 3: 
-                    say('Command is unknown. Type \'help\' if you need to.');
+                case 3:
+                    say("Command is unknown. Type 'help' if you need to.");
                     break;
             }
         }
